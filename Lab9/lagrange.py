f25ce3859573a122fdb22cae8a618383fb4fc0df
Martini <marcino064@o2.pl>
2024-05-19 22:54:48 +0200

    Init Lab9

--- Lab9/lagrange.py
+++ Lab9/lagrange.py
@@ -1,32 +1,42 @@
 import sympy as sp
 
+def lagrange_method(f, constraints, variables):
+    # Definiowanie mnożników Lagrange'a
+    lambdas = sp.symbols(f'lambda1:{len(constraints)+1}')
+    
+    # Definiowanie funkcji Lagrange'a
+    L = f + sum(lambdas[i] * constraints[i] for i in range(len(constraints)))
 
-def lagrange_multiplier(f, constraints, variables):
-    # Tworzenie mnożników Lagrange'a
-    lambdas = sp.symbols('lambda1:%d' % (len(constraints) + 1))
-    L = f  # Funkcja Lagrange'a zaczyna się od funkcji celu
+    # Obliczanie pochodnych cząstkowych
+    derivatives = [sp.diff(L, var) for var in variables] + constraints
 
-    # Dodawanie ograniczeń z mnożnikami do funkcji Lagrange'a
-    for lam, h in zip(lambdas, constraints):
-        L += lam * h
-
-    # Wyznaczanie pochodnych względem wszystkich zmiennych i mnożników
-    grad = [sp.diff(L, var) for var in variables + list(lambdas)]
-
-    # Rozwiązywanie równań: pochodne ustawione na 0
-    solutions = sp.solve(grad, variables + list(lambdas), dict=True)
-
+    # Rozwiązywanie układu równań
+    solutions = sp.solve(derivatives, variables + list(lambdas), dict=True)
+    
     return solutions
 
+def print_solution(solution):
+    for sol in solution:
+        for var, val in sol.items():
+            print(f"{var} = {val.evalf(n=1)}")
+        print()
 
-# Przykład: prostopadłościan o maksymalnej objętości
+# Przykład 1
+x1, x2 = sp.symbols('x1 x2')
+f1 = x1**2 + x2**2
+constraint1 = [2*x1 + x2 - 2]
+variables1 = [x1, x2]
+
+solution1 = lagrange_method(f1, constraint1, variables1)
+print("Przykład 1:")
+print_solution(solution1)
+
+# Przykład 2
 x, y, h = sp.symbols('x y h')
-f = x * y * h  # funkcja celu: objętość prostopadłościanu
-constraint = x + y + h - 6  # ograniczenie z drutu
+f2 = x * y * h
+constraint2 = [6 - x - y - h]
+variables2 = [x, y, h]
 
-# Rozwiązanie
-results = lagrange_multiplier(f, [constraint], [x, y, h])
-
-# Filtrowanie wyników dla dodatnich wartości x, y, h
-valid_results = [res for res in results if all(res[var] > 0 for var in [x, y, h])]
-print(valid_results)
+solution2 = lagrange_method(f2, constraint2, variables2)
+print("Przykład 2:")
+print_solution(solution2)
