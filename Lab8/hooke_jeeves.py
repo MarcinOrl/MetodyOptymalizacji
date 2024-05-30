0bd0288c569a89491cb293e5d95a4189aabd3102
Martini <marcino064@o2.pl>
2024-05-30 17:51:51 +0200

    Init Hooke-Jeeves

--- Lab8/hooke_jeeves.py
+++ Lab8/hooke_jeeves.py
@@ -1,72 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-# Parametry początkowe z przykładu
-x_B = np.array([-0.5, 1])  # Punkt bazowy
-xi = np.array([[1, 0], [0, 1]])  # Baza wektorów ortogonalnych
-e = 0.5  # Początkowa długość kroku
-beta = 0.5  # Współczynnik zmniejszenia kroku
-epsilon = 0.0001  # Dokładność obliczeń
-n = 2  # Liczba zmiennych niezależnych
+f = lambda x_1, x_2: x_2 * x_2 * x_2 + 3 * x_1 * x_1 - x_1 * x_2 - 2 * x_1 + 5
+x0 = [1, 1]
+e = 0.5
+beta = 0.5
+epsilon = 0.01
+n = 2
 
-# Definicja funkcji celu z przykładu
-def f(x):
-    x1, x2 = x
-    #return 2.5 * (x1**2 - x2)**2 + (1 - x1)**2
-    return x2 * x2 * x2 + 3 * x1 * x1 - x1 * x2 - 2 * x1 + 5
+def hooke_jeeves(x0, n, e, beta, epsilon, max_iter=100):
+    x_base = np.array(x0)
+    x_new = x_base.copy()
+    xi = np.eye(n)
+    path = [x_base.copy()]
 
-# Implementacja metody Hooke’a-Jeevesa
-def hooke_jeeves(x_B, e, beta, epsilon, f):
-    def trial_step(x, e, xi):
+    for _ in range(max_iter):
+        x_before = x_new.copy()
+        improved = False
+
+        # Exploratory move
         for j in range(n):
-            f_x = f(x)
-            x_trial = x + e * xi[j]
-            f_trial = f(x_trial)
-            if f_trial < f_x:
-                x = x_trial
+            for sign in [1, -1]:
+                x_trial = x_new + sign * e * xi[j]
+                if f(*x_trial) < f(*x_new):
+                    x_new = x_trial
+                    improved = True
+                    break
+            if improved:
+                break
+
+        if improved:
+            # Pattern move
+            x_step = 2 * x_new - x_base
+            if f(*x_step) < f(*x_base):
+                x_base = x_new
+                x_new = x_step
             else:
-                x_trial = x - e * xi[j]
-                f_trial = f(x_trial)
-                if f_trial < f_x:
-                    x = x_trial
-        return x
-
-    x_B0 = x_B.copy()  # Punkt bazowy z poprzedniej iteracji
-    x = x_B.copy()  # Punkt startowy
-    path = [x.copy()]  # Ścieżka do wizualizacji
-
-    while e > epsilon:
-        x_new = trial_step(x, e, xi)
-        if f(x_new) < f(x):
-            x_B0 = x_B.copy()
-            x_B = x_new.copy()
-            x = 2 * x_B - x_B0  # Krok roboczy
+                x_base = x_new
         else:
-            e = beta * e
-            x = x_B.copy()
-        path.append(x.copy())
-    return x, path
+            e *= beta
 
-# Wywołanie metody Hooke’a-Jeevesa
-ext, path = hooke_jeeves(x_B, e, beta, epsilon, f)
-ext_rounded = np.round(ext, 8)
+        path.append(x_new.copy())
+
+        if e < epsilon:
+            break
+
+    return x_new, path
+
+opt_point, path = hooke_jeeves(x0, n, e, beta, epsilon)
+path = np.array(path)
+
+print("Optymalny punkt:", opt_point)
+print("Wartość funkcji w punkcie optymalnym:", f(*opt_point))
 
 # Wizualizacja
-path = np.array(path)
+x = np.linspace(0.2, 1.1, 400)
+y = np.linspace(-0.2, 1.4, 400)
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+
 plt.figure(figsize=(8, 6))
-x_range = np.linspace(-1, 2, 400)
-y_range = np.linspace(-1, 2, 400)
-X, Y = np.meshgrid(x_range, y_range)
-Z = f([X, Y])
-plt.contour(X, Y, Z, levels=50)
-plt.plot(path[:, 0], path[:, 1], 'o-', color='blue', label='Ścieżka iteracji')
-plt.plot(ext[0], ext[1], 'ro', label='Punkt ekstremum')
-plt.xlabel('x1')
-plt.ylabel('x2')
+plt.contour(X, Y, Z, levels=10)
+
+# Gradually change colors along the path
+colors = cm.viridis(np.linspace(0, 1.5, len(path)))
+for i in range(len(path)-1):
+    plt.plot(path[i:i+2, 0], path[i:i+2, 1], 'o--', color=colors[i], label='Ścieżka optymalizacji' if i == 0 else "")
+
+plt.scatter([opt_point[0]], [opt_point[1]], c='red', marker='o', label='Optymalny punkt', zorder=2)
 plt.title('Metoda Hooke’a-Jeevesa')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.legend()
 plt.grid(True)
 plt.show()
-
-# Wyniki
-print(f"Znalezione minimum: {ext_rounded}")
