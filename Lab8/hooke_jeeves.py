397a5a032eb00926462b065a233df9646df9bee6
Martini <marcino064@o2.pl>
2024-06-16 23:03:21 +0200

    Hooke_jeeves fix

--- Lab8/hooke_jeeves.py
+++ Lab8/hooke_jeeves.py
@@ -2,79 +2,108 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-f = lambda x_1, x_2: x_2 * x_2 * x_2 + 3 * x_1 * x_1 - x_1 * x_2 - 2 * x_1 + 5
-x0 = [1, 1]
+
+def function(x, y):
+    # return y * y * y + 3 * x * x - x * y - 2 * x + 5
+    return 2.5 * (x**2 - y) ** 2 + (1 - x) ** 2
+
+
+xb = xb0 = x0 = [-0.5, 1]
+wo1 = np.array([1, 0])
+wo2 = np.array([0, 1])
 e = 0.5
 beta = 0.5
-epsilon = 0.00001
 n = 2
+epsilon = 0.00001
 
-def hooke_jeeves(x0, n, e, beta, epsilon, max_iter=100):
+
+def hooke_jeeves(x0, xb, e, wo1, wo2, n, epsilon, beta):
     iter = 0
-    x_base = np.array(x0)
-    x_new = x_base.copy()
-    xi = np.eye(n)
-    path = [x_base.copy()]
+    path = [x0.copy()]
 
-    for _ in range(max_iter):
-        x_before = x_new.copy()
-        improved = False
+    while True:
+        j = 1
+        f0 = function(x0[0], x0[1])
+        fb = function(xb[0], xb[1])
 
-        # Exploratory move
-        for j in range(n):
-            for sign in [1, -1]:
-                x_trial = x_new + sign * e * xi[j]
-                if f(*x_trial) < f(*x_new):
-                    x_new = x_trial
-                    improved = True
-                    break
-            if improved:
-                break
+        x_next = x0 + e * wo1
+        f = function(x_next[0], x_next[1])
 
-        if improved:
-            if iter == 15: break
-            iter += 1
-            # Pattern move
-            x_step = 2 * x_new - x_base
-            if f(*x_step) < f(*x_base):
-                x_base = x_new
-                x_new = x_step
-            else:
-                x_base = x_new
+        if f < f0:
+            f0 = f
         else:
-            e *= beta
+            x_next = x_next - 2 * e * wo1
+            f = function((x_next[0]), x_next[1])
 
-        path.append(x_new.copy())
+            if f < f0:
+                f0 = f
+            else:
+                x_next = x_next + e * wo1
+
+        if j != n:
+            j += 1
+
+            x_next = x_next + e * wo2
+            f = function((x_next[0]), x_next[1])
+
+            if f < f0:
+                f0 = f
+            else:
+                x_next = x_next - 2 * e * wo2
+                f = function((x_next[0]), x_next[1])
+
+                if f < f0:
+                    f0 = f
+                else:
+                    x_next = x_next + e * wo2
+
+            if fb > f0:
+                xb0 = xb
+                xb = x_next
+                x0 = 2 * xb - xb0
+                print(x0)
+            elif e > epsilon:
+                e = beta * e
+                x0 = xb
+
+        path.append(x_next.copy())
+        iter += 1
 
         if e < epsilon:
-            break
+            return x_next[0], x_next[1], iter, path
 
-    return x_new, path, iter
 
-opt_point, path, iterations = hooke_jeeves(x0, n, e, beta, epsilon)
+x, y, iterations, path = hooke_jeeves(x0, xb, e, wo1, wo2, n, epsilon, beta)
 path = np.array(path)
 
-print(f"Optymalny punkt:, {opt_point}, znalezione w {iterations} iteracjach.")
-print("Wartość funkcji w punkcie optymalnym:", f(*opt_point))
+print(f"Optymalny punkt:, [{x}, {y}], znaleziony w {iterations} iteracjach.")
+print("Wartość funkcji w punkcie optymalnym:", function(x, y))
 
 # Wizualizacja
-x = np.linspace(0, 1.5, 400)
-y = np.linspace(-0.2, 1.5, 400)
-X, Y = np.meshgrid(x, y)
-Z = f(X, Y)
-
 plt.figure(figsize=(8, 6))
-plt.contour(X, Y, Z, levels=20)
 
 # Gradually change colors along the path
 colors = cm.viridis(np.linspace(0, 1.5, len(path)))
-for i in range(len(path)-1):
-    plt.plot(path[i:i+2, 0], path[i:i+2, 1], 'o--', color=colors[i], label='Ścieżka optymalizacji' if i == 0 else "")
+for i in range(len(path) - 1):
+    plt.plot(
+        path[i : i + 2, 0],
+        path[i : i + 2, 1],
+        "o--",
+        color=colors[i],
+        label="Ścieżka optymalizacji" if i == 0 else "",
+    )
 
-plt.scatter([opt_point[0]], [opt_point[1]], c='red', marker='o', label='Optymalny punkt', zorder=2)
-plt.title('Metoda Hooke’a-Jeevesa')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.scatter(
+    [x],
+    [y],
+    c="red",
+    marker="o",
+    label="Optymalny punkt",
+    zorder=2,
+)
+plt.title("Metoda Hooke’a-Jeevesa")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.legend()
 plt.grid(True)
 plt.show()
